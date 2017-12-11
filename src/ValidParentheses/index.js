99134af190031b0fa9007d28e27e4c1e46e111b3
le.zheng <le.zheng@ef.com>
2017-12-11 11:28:54 +0800

    project boostrap

---
+++ src/ValidParentheses/index.js
@@ -0,0 +1,9 @@
+/* Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not. */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+export const isValid = function(s) {
+    return true;
+}
